b004c19afe215a6c4cba1a00529487daffc43adb
michal-wolski <michal.wolski130@gmail.com>
2022-03-22 23:51:21 +0100

    Timer alert

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,21 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates, defaultDate) {
+        if (selectedDates[0] < new Date()){
+            alert("Please choose a date in the future")
+        }
+        else {
+            console.log(selectedDates[0]);
+            console.log(defaultDate);
+        }
+    },
+    
+  };
+
+  flatpickr("#datetime-picker", options);
